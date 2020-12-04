0b97503fe35ce8b207f016e54ce4cb015e963db0
Moe98 <moeadele98@gmail.com>
2020-12-04 21:02:40 +0200

    cleaned code

--- project.py
+++ project.py
@@ -7,35 +7,39 @@ maxDim = 10
 
 
 class Rectangle:
-    def __init__(self, minArea, model, width=0, height=0):
-        self.minArea = minArea
+    def __init__(self, minArea=1, width=0, height=0):
+        # Name the variable names in the model properly.
         self.width = model.NewIntVar(1, maxDim, 'w')
         self.height = model.NewIntVar(1, maxDim, 'h')
-        self.area = model.NewIntVar(minArea, maxDim*maxDim, 'area')
+        self.area = model.NewIntVar(minArea, maxDim * maxDim, 'area')
         self.startRow = model.NewIntVar(0, maxDim, 'startRow')
         self.startCol = model.NewIntVar(0, maxDim, 'startCol')
         self.endRow = model.NewIntVar(0, maxDim, 'endRow')
         self.endCol = model.NewIntVar(0, maxDim, 'endCol')
+
+        self.addGenericConstraints(width, height)
+
+    def addGenericConstraints(self, width, height):
+        # We call the methods if the method is invoked with these parameters.
         if(width > 0):
-            self.addWidth(width, model)
+            self.addWidth(width)
         if(height > 0):
-            self.addHeight(height, model)
+            self.addHeight(height)
+
+        model.Add(self.width == self.endCol-self.startCol)
+        model.Add(self.height == self.endRow-self.startRow)
         model.AddMultiplicationEquality(self.area, [self.width, self.height])
 
-        model.Add(self.area >= minArea)
-    # def __init__(self, startX, startY, width, height):
-    #     self.width = width
-    #     self.height = height
-
     def toString(self):
-        print("Rectangle coordinates: (%d,%d)" % (self.startX, self.startY))
+        print("Rectangle coordinates: (%d,%d)" %
+              (self.startRow, self.startCol))
         print("Rectangle width: %d, Rectangle height: %d" %
               (self.width, self.height))
 
-    def addWidth(self, width, model):
+    def addWidth(self, width):
         model.Add(self.width == width)
 
-    def addHeight(self, height, model):
+    def addHeight(self, height):
         model.Add(self.height == height)
 
     def getLeft(self):
@@ -51,8 +55,9 @@ class Rectangle:
         return self.endRow
 
 
-def VisualizeApartments(model, rooms):
-    visualizedApartment = [[0 for i in range(10)] for j in range(10)]
+def VisualizeApartments(apartment, rooms):
+    visualizedApartment = [[0 for i in range(solver.Value(
+        apartment.width))] for j in range(solver.Value(apartment.height))]
     for index, room in enumerate(rooms):
         startRow = solver.Value(room.startRow)
         startCol = solver.Value(room.startCol)
@@ -65,19 +70,27 @@ def VisualizeApartments(model, rooms):
     for row in visualizedApartment:
         print(row)
 
+# This method sets the relation between the start and end (rows/columns) by adding the |AddNoOverlap2D| constraint to the model.
 
-def addNoIntersectionConstraint(model, rooms):
-    # for i in range(len(rooms)):
-    #     for j in range(i):
-    #         A = rooms[i]
-    #         B = rooms[j]
-    #         model.Add(A.getLeft() < B.getRight()
-    #                   and A.getRight() > B.getLeft() and A.getTop() > B.getBottom() and A.getBottom() < B.getTop())
+
+def AddNoIntersectionConstraint(rooms):
     rowIntervals = [model.NewIntervalVar(
-        room.getTop(), room.height, room.getBottom(), 'room 1 ') for room in rooms]
+        room.getTop(), room.height, room.getBottom(), 'room %d' % (roomNum + 1)) for roomNum, room in enumerate(rooms)]
     colIntervals = [model.NewIntervalVar(
-        room.getLeft(), room.width, room.getRight(), 'room 1 ') for room in rooms]
+        room.getLeft(), room.width, room.getRight(), 'room %d' % (roomNum + 1)) for roomNum, room in enumerate(rooms)]
     model.AddNoOverlap2D(colIntervals, rowIntervals)
+
+
+def GetBorders(rooms):
+    leftBorders = [rooms[i].getLeft() for i in range(nOfRooms)]
+    rightBorders = [rooms[i].getRight() for i in range(nOfRooms)]
+    topBorders = [rooms[i].getTop() for i in range(nOfRooms)]
+    bottomBorders = [rooms[i].getBottom() for i in range(nOfRooms)]
+    return leftBorders, rightBorders, topBorders, bottomBorders
+
+########################   Main Method Starts Here   ########################
+
+########################   Process Future Input Here ########################
 
 
 nOfApartments = 1
@@ -85,46 +98,34 @@ nOfRooms = 4
 rooms = []
 
 
-# optional width, height => width, height
 model = cp_model.CpModel()
 minArea = [i for i in range(nOfRooms)]
+
 for i in range(nOfRooms):
-    rooms.append(Rectangle(minArea[i], model, 2 if i == 0 else 0))
-# for i in start:
-#     print("%s = %i" % (i, solver.Value(i)))
-addNoIntersectionConstraint(model, rooms)
+    rooms.append(Rectangle(minArea[i], 2 if i == 0 else 0, 3 if i == 1 else 2))
 
 
-leftBorders = [rooms[i].getLeft() for i in range(nOfRooms)]
-rightBorders = [rooms[i].getRight() for i in range(nOfRooms)]
-upBorders = [rooms[i].getTop() for i in range(nOfRooms)]
-downBorders = [rooms[i].getBottom() for i in range(nOfRooms)]
-apartment_startRow = model.NewIntVar(0, maxDim, 'appar_start_row')
-apartment_endRow = model.NewIntVar(0, maxDim, 'appar_end_row')
-apartment_startCol = model.NewIntVar(0, maxDim, 'appar_start_col')
-apartment_endCol = model.NewIntVar(0, maxDim, 'appar_end_scol')
-model.AddMinEquality(apartment_startCol, leftBorders)
-model.AddMaxEquality(apartment_endCol, rightBorders)
-model.AddMinEquality(apartment_startRow, upBorders)
-model.AddMaxEquality(apartment_endRow, downBorders)
-apartment_width = model.NewIntVar(0, maxDim, 'xx')
-model.Add(apartment_width == apartment_endCol-apartment_startCol)
+########################   Process Future Input Here ########################
 
-apartment_height = model.NewIntVar(0, maxDim, 'xx')
-model.Add(apartment_height == apartment_endRow-apartment_startRow)
+AddNoIntersectionConstraint(rooms)
 
-appartment_area = model.NewIntVar(0, maxDim, 'area')
-model.AddMultiplicationEquality(appartment_area, [
-    apartment_width, apartment_height])
+apartment = Rectangle()
 
-model.Minimize(appartment_area)
+leftBorders, rightBorders, topBorders, bottomBorders = GetBorders(rooms)
+
+model.AddMinEquality(apartment.getLeft(), leftBorders)
+model.AddMaxEquality(apartment.getRight(), rightBorders)
+model.AddMinEquality(apartment.getTop(), topBorders)
+model.AddMaxEquality(apartment.getBottom(), bottomBorders)
+
+
+model.Minimize(apartment.area)
 solver = cp_model.CpSolver()
 status = solver.Solve(model)
 print(solver.StatusName())
-print(solver.Value(appartment_area))
+print(solver.Value(apartment.area))
 for room in rooms:
     print(solver.Value(room.startRow), solver.Value(room.startCol),
           solver.Value(room.width), solver.Value(room.height))
-print(solver.Value(appartment_area))
-VisualizeApartments(model, rooms)
-#
+
+VisualizeApartments(apartment, rooms)
